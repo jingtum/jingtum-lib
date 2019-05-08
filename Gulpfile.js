8da09cac2cf29a74bffab29c94736d1def8a010e
wudanjs <1872776189@qq.com>
2019-05-08 15:40:05 +0800

    init

---
+++ Gulpfile.js
@@ -0,0 +1,21 @@
+// gulp jingtum base lib to front enabled js lib
+'use strict';
+
+var gulp = require('gulp');
+var webpack = require('webpack-stream');
+var webpack_config = require('./webpack.config');
+var prod_webpack_config = require('./prod.webpack.config');
+
+
+gulp.task('dev', function() {
+	return gulp.src('index.js')
+		.pipe(webpack(webpack_config))
+		.pipe(gulp.dest('dist/'))
+});
+
+gulp.task('production', function() {
+	return gulp.src('index.js')
+		.pipe(webpack(prod_webpack_config))
+		.pipe(gulp.dest('dist/'))
+});
+
